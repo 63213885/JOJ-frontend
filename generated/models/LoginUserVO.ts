274695cfcf8ad223574388d602dd7f5b226e3955
jzz <63213885@qq.com>
2026-04-12 23:55:32 +0800

    刷新后依旧显示登录状态

---
+++ generated/models/LoginUserVO.ts
@@ -0,0 +1,16 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type LoginUserVO = {
+    account?: string;
+    avatarUrl?: string;
+    bio?: string;
+    email?: string;
+    lastLoginIp?: string;
+    lastLoginTime?: string;
+    phone?: string;
+    role?: string;
+    school?: string;
+};
+
